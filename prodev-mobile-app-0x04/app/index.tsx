e90683bfceace64b348f854b8e15d271be413819
Meron Solomon <meronsolomondana@gmail.com>
2025-07-20 17:30:48 +0300

    first commit

---
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -0,0 +1,45 @@
+import {
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { styles } from "@/styles/_mainstyle";
+import { BACKGROUNDIMAGE, HEROLOGO } from "@/constants";
+import { router } from "expo-router";
+
+export default function Home() {
+  return (
+    <ImageBackground
+      source={BACKGROUNDIMAGE}
+      style={styles.backgroundImageContainer}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={HEROLOGO} />
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Welcome to ProDev</Text>
+      </View>
+      <View style={styles.titleSubTextContainer}>
+        <Text style={styles.titleSubText}>Explore your potential.</Text>
+      </View>
+      <View style={{ marginTop: 50 }}>
+        <View style={styles.buttonGroup}>
+          <TouchableOpacity
+            style={styles.buttonPrimary}
+            onPress={() => router.push("/Sign")}
+          >
+            <Text style={styles.buttonPrimaryText}>Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonSecondary}
+            onPress={() => router.push("/join")}
+          >
+            <Text style={styles.buttonSecondaryText}>Join Now</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+}
